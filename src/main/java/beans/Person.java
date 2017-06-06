dc9079599afdf4cc24b8a2cafd293755e30affcb
WillyLu326 <luzh0326@gmail.com>
2017-06-05 22:22:54 -0400

    update Person Bean

--- src/main/java/beans/Person.java
+++ src/main/java/beans/Person.java
@@ -1,6 +1,16 @@
 package beans;
 
 public class Person {
+	
+	private int id;
+	
+	private String name;
+	
+	public Person(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
 	public void speak() {
 		System.out.println("Hi, I'm a person");
 	}
