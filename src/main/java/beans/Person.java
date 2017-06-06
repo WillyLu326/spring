aac4ffd30fbf7c0a674cd327ab74dc6e7cf83754
WillyLu326 <luzh0326@gmail.com>
2017-06-05 22:27:48 -0400

    Java Bean

--- src/main/java/beans/Person.java
+++ src/main/java/beans/Person.java
@@ -6,6 +6,8 @@ public class Person {
 	
 	private String name;
 	
+	public Person() {}
+	
 	public Person(int id, String name) {
 		this.id = id;
 		this.name = name;
@@ -14,4 +16,10 @@ public class Person {
 	public void speak() {
 		System.out.println("Hi, I'm a person");
 	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
 }
--- src/main/java/spring/App.java
+++ src/main/java/spring/App.java
@@ -10,10 +10,13 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		@SuppressWarnings("resource")
 		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
 		
 		Person person = (Person)context.getBean("person");
 		person.speak();
+		
+		System.out.println(person);
 	}
 
 }
